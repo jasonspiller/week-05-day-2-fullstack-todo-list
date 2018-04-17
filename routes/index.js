2fdd966cdac7b0758a43c49eb0485a260fc72179
Jason Spiller <jason@thetargetlab.com>
2018-04-17 13:32:11 -0600

    404 complete.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+// home page
 router.get('/', function(req, res, next) {
 	let data = {title: 'Welcome!'};
 	res.render('index', data);
 });
+
+// catch all 404
+app.get('*', (req, res) => {
+	res.render('404', {});
+});
+
 
 module.exports = router;
